17bd292c43fed1f21c50db0405e5d02b11619ecc
Rafael Sanabria <rafael.d.sanabria@gmail.com>
2023-07-30 18:38:04 -0500

    Small additions

--- build_readme.py
+++ build_readme.py
@@ -53,19 +53,33 @@ def generate_film_maps(df):
 def main():
     df = pd.read_csv("data/films.csv")
     generate_film_maps(df)
-    last_movie = df.sort_values("seen_on").iloc[-1]["title"]
+    last_movie = df.sort_values("seen_on").iloc[-1]
+    movies_seen = df.shape[0]
+    movies_in_collection = df.query("rating != 'Not for me'").shape[0]
+    pct_in_collection = round(movies_in_collection * 100 / movies_seen, 1)
 
     md = ""  # Markdown file string
     md += "# My Collections\n\n"
     md += "## Movies\n\n"
-    md += f"- Movies I've seen: {df.shape[0]}\n"
-    md += f"- Last movie I saw: {last_movie}\n\n"
-    md += "### Ranked\n"
-    for rating in ["Favorite", "Great", "Good"]:
-        md += f"\n#### {rating}\n\n"
-        subdf = df.query(f"rating == '{rating}'").sort_values(by="director")
-        md += subdf[["director", "title", "year", "country"]].to_markdown(index=False)
-        md += "\n"
+    md += f"- Movies I've seen: {movies_seen}\n"
+    md += f"- Movies in my collection: {movies_in_collection} ({pct_in_collection}%)\n"
+    md += f"- Last movie I saw: {last_movie['title']} - {last_movie['director']}\n"
+    md += f"- I've seen movies from {df['country'].nunique()} countries\n"
+    md += "\n### Ranked\n"
+    for rating in ["Favorite", "Great", "Good", "Not for me"]:
+        md_cols = ["director", "title", "year", "country"]
+        dfsub = df.query(f"rating == '{rating}'").sort_values(by="director")[md_cols]
+        if rating == "Not for me":
+            md += "\n<details>\n"
+            md += f"<summary>### Not for me ({dfsub.shape[0]} movies)</summary>\n"
+            md += dfsub.to_markdown(index=False)
+            md += "\n</details>\n"
+        else:
+            md += f"\n#### {rating} ({dfsub.shape[0]} movies)\n\n"
+            md += dfsub.to_markdown(index=False)
+            md += "\n"
+
+    # by country
     md += "\n### Movies by country\n\n"
     md += "<picture>\n"
     md += """  <source media="(prefers-color-scheme: dark)" srcset="figures/films_map_plotly_dark.png">\n"""
