249b93324029dae8bf465940d9c64e417ff57a0d
Rafael Sanabria <rafael.d.sanabria@gmail.com>
2023-07-30 20:52:59 -0500

    Add movies per month plot

--- build_readme.py
+++ build_readme.py
@@ -1,5 +1,6 @@
 import numpy as np
 import pandas as pd
+import plotly.express as px
 import plotly.graph_objects as go
 
 
@@ -50,8 +51,35 @@ def generate_film_maps(df):
         fig.write_image(f"figures/films_map_{theme}.png", width=1600, height=900)
 
 
+def generate_movies_by_month_plots(df):
+    movies_by_month = (
+        df["seen_on"]
+        .str.slice(stop=7)
+        .value_counts(dropna=False)
+        .reset_index()
+        .sort_values(by="seen_on")
+    )
+    for theme in ["plotly_white", "plotly_dark"]:
+        fig = px.bar(movies_by_month, x="seen_on", y="count", template=theme)
+        fig.update_xaxes(
+            dtick="M1", tickformat="%b\n%Y", title_text=None, tickfont={"size": 22}
+        )
+        fig.update_yaxes(title_text=None, tickfont={"size": 22}, range=[0, 12])
+        fig.write_image(f"figures/movies_by_month_{theme}.png", width=1600, height=900)
+
+
+def add_image_with_dark_mode(light_img, dark_img, alt) -> str:
+    img_md = "<picture>\n"
+    img_md += f"""  <source media="(prefers-color-scheme: dark)" srcset="figures/{dark_img}">\n"""
+    img_md += f"""  <source media="(prefers-color-scheme: light)" srcset="figures/{light_img}">\n"""
+    img_md += f"""  <img alt="{alt}" src="figures/{light_img}">\n"""
+    img_md += "</picture>\n"
+    return img_md
+
+
 def main():
     df = pd.read_csv("data/films.csv")
+    generate_movies_by_month_plots(df)
     generate_film_maps(df)
     last_movie = df.sort_values("seen_on").iloc[-1]
     movies_seen = df.shape[0]
@@ -65,10 +93,15 @@ def main():
     md += f"- Movies in my collection: {movies_in_collection} ({pct_in_collection}%)\n"
     md += f"- Last movie I saw: {last_movie['title']} - {last_movie['director']}\n"
     md += f"- I've seen movies from {df['country'].nunique()} countries\n"
+
+    # tables by ranking
     md += "\n### Ranked\n"
     for rating in ["Favorite", "Great", "Good", "Not for me"]:
         md_cols = ["director", "title", "year", "country"]
-        dfsub = df.query(f"rating == '{rating}'").sort_values(by="director")[md_cols]
+        dfsub = (
+            df.query(f"rating == '{rating}'").sort_values(by="director")[md_cols].copy()
+        )
+        dfsub.columns = dfsub.columns.map(str.capitalize)
         if rating == "Not for me":
             md += "\n<details>\n"
             md += f"<summary>Not for me ({dfsub.shape[0]} movies)</summary>\n\n"
@@ -81,11 +114,19 @@ def main():
 
     # by country
     md += "\n### Movies by country\n\n"
-    md += "<picture>\n"
-    md += """  <source media="(prefers-color-scheme: dark)" srcset="figures/films_map_plotly_dark.png">\n"""
-    md += """  <source media="(prefers-color-scheme: light)" srcset="figures/films_map_plotly.png">\n"""
-    md += """  <img alt="Frequency of films by country choropleth map" src="figures/films_map_plotly.png">\n"""
-    md += "</picture>\n"
+    md += add_image_with_dark_mode(
+        dark_img="films_map_plotly_dark.png",
+        light_img="films_map_plotly.png",
+        alt="Frequency of films by country choropleth map",
+    )
+
+    # movies seen by month
+    md += "\n### Count of movies I've seen per month\n\n"
+    md += add_image_with_dark_mode(
+        dark_img="movies_by_month_plotly_dark.png",
+        light_img="movies_by_month_plotly_white.png",
+        alt="Count of movies I've seen per month",
+    )
 
     with open("README.md", "w") as readme:
         readme.write(md)
